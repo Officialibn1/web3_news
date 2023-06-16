5d715a2b5722e71f2f72d03e5b8607e422710b43
Officialibn1 <officialibn001@gmail.com>
2023-06-16 22:35:42 +0100

    Recomendations section complete

--- src/components/Recomendations.jsx
+++ src/components/Recomendations.jsx
@@ -1,7 +1,24 @@
 const Recomendations = () => {
     return (
         <div className="recomendations">
+            <div className="card">
+                <p>Recomended for Ibn</p>
+                <img src="../../public/img/laptop.jpg" alt="" />
+            </div>
+
+            <div className="card">
+                <p>Porpular in ibn&apos;s App</p>
+                <img src="../../public/img/laptop.jpg" alt="" />
+            </div>
             
+            <div className="card">
+                <p>Editors Choice</p>
+                <img src="../../public/img/laptop.jpg" alt="" />
+            </div>
+
+            <div className="more">
+                <p>See More</p>
+            </div>
         </div>
     );
 }
