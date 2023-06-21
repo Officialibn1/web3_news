c4b240addd177511c9d91fda5eb83f0046c3b9ca
Officialibn1 <officialibn001@gmail.com>
2023-06-21 12:29:49 +0100

    Started Frontend Challange

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import MainSection from "./components/MainSection";
-import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main>
-        <Navbar />
-
-        <MainSection />
+      <h1>FrontEnd Mentor</h1>
     </main>
   );
 }
